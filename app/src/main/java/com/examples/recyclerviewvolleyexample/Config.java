f0d13cc56f6162d6220e2eeb692b8ea80c1074ed
janipasha <janipasha1308@gmail.com>
2017-04-03 14:07:07 +0530

    Initial commit

---
+++ app/src/main/java/com/examples/recyclerviewvolleyexample/Config.java
@@ -0,0 +1,20 @@
+package com.examples.recyclerviewvolleyexample;
+
+/**
+ * Created by Jani on 01-04-2017.
+ */
+
+public class Config {
+    //URL of my API
+    public static final String DATA_URL = "https://api.myjson.com/bins/pq3u7";
+
+    //Tags for my JSON
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_RANK = "rank";
+    public static final String TAG_REAL_NAME = "realName";
+    public static final String TAG_CREATED_BY = "createdBy";
+    public static final String TAG_FIRST_APPEARANCE = "firstAppearance";
+    public static final String TAG_POWERS = "powers";
+
+}
